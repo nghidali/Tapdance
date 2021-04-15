9023fee48bd058c143379d544b2578a6f1609f0f
sojure <soteriareid2021@u.northwestern.edu>
2021-04-14 23:26:06 -0500

    read in data

---
+++ tapdance_eda.R
@@ -0,0 +1,8 @@
+
+# Project EDA -------------------------------------------------------------
+
+# loading packages
+library(tidyverse)
+library(tidymodels)
+
+tap_dat <- read_csv("data/tap_dat.csv")
