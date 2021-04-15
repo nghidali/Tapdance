48b4c81024fb10a9b52371147a476ece3124faa7
sojure <soteriareid2021@u.northwestern.edu>
2021-04-15 00:05:57 -0500

    Skimmed dataset and outcome variable

--- tapdance_eda.R
+++ tapdance_eda.R
@@ -4,5 +4,16 @@
 # loading packages
 library(tidyverse)
 library(tidymodels)
+library(skimr)
+library(janitor)
 
-tap_dat <- read_csv("data/tap_dat.csv")
+# reading in data
+tap_dat <- read_csv("data/tap_dat.csv") %>% 
+  clean_names()
+
+# skim full dataset
+skim(tap_dat) %>% view()
+
+# skim outcome variable
+skim(tap_dat$sector_type)
+
